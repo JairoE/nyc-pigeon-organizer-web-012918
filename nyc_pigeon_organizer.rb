dd9f9a48f1e53e384b0b832b5876383ba1cb7ff3
Jairo Espinosa <jairoespinosa95@gmail.com>
2018-01-24 02:58:56 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  pigeon_list = {}
+
+  data.each do |attribute, kinds|
+    kinds.each do |option, names|
+      names.each do |name|
+        if !pigeon_list.keys.include?(name)
+          pigeon_list[name] = {attribute => [option]}
+        elsif !pigeon_list[name].keys.include?(attribute)
+          pigeon_list[name][attriube] = [option]
+        else
+          pigeon_list[name][attribute].push(option)
+        end
+      end
+    end
+  end
+
+  pigeon_list
 end
